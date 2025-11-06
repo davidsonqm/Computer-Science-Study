491e976f5eba1ede1fce3f0d1d52663fe9a78d41
davidsonqm <davidsonquaresma@hotmail.com>
2025-11-06 10:50:57 -0300

    feat: created files,variable.c

---
+++ study/programing-C/variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int age = 25;
+	// int para variavel de numero inteiro e para fazer print? 
+	// fazer %d que dizer decimal.
+	
+	float gpa = 2.5;
+	// float serve para numeros com ponto flutuante e para fazer print?
+	// fazer %f que dizer float porém ele adicions 6 numeros de pontos flutuantes
+
+	double pi = 3.56581818954981;
+	// long float com double faz com quer numeros flutuande de 15 a 16 digitos
+	// fazer %lf para printar na leta .e quantidade de digitos 
+	
+	
+
+	return 0;
+}
